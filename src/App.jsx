a5c14fa38b3547226ec79f4c0a4e5c5bab8d7c65
Kartik <kartiktripathi103@gmail.com>
2026-02-05 19:36:29 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import RegisterPlaceholder from './pages/RegisterPlaceholder';
 import './styles/global.css';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<RegisterPlaceholder />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterPlaceholder />} />
+        </Routes>
     );
 }
 
